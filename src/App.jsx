3be0e11b0e17ba62bf77af1f24bc3a4f92c16d31
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-09-24 18:52:09 -0300

    We are fetching movies successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect ,useState } from 'react'
 import Buscar from './componentes/Buscar'
 
+const API_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 const App = () => {
 
   const [termoBusca,setTermoBusca] = useState("")
+
+  const [errMsg,setErrMsg] = useState("")
+  const fetch_movies = async () => {
+    try {
+        const endpoint = `${API_URL}/discover/movie?sort_by=popularity.desc`
+        
+        const resp = await fetch(endpoint,API_OPTS)
+
+        alert(resp)
+    } catch (error) {
+      console.error(`Erro ao dar fetch nos filmes: ${error}`)
+      setErrMsg("Erro ao dar fetch. Por favor volte mais tarde.")
+    }
+  }
+
+  useEffect(() => {
+      fetch_movies()
+  },)
+
+
   return (
     <main>
       <div className='pattern'>
@@ -11,9 +42,16 @@ const App = () => {
             <header>
               <img src="./hero.png" alt="" />
               <h1>Encontre <span className="text-gradient">Filmes </span>que você gosta sem se aborecer</h1>
+                  <Buscar termoBusca={termoBusca} setTermoBusca={setTermoBusca}/>
             </header>
+            <section className="all-movies">
+              <h2>Todos os filmes</h2>
 
-            <Buscar termoBusca={termoBusca} setTermoBusca={setTermoBusca}/>
+              {errMsg && <p className='text-red-700'>{errMsg}</p>}
+            </section>
+
+
+
             <h1 className="text-white">{termoBusca}</h1>
           </div>
       
