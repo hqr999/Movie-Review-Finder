ce744b6405ba34619ba2df773e045819cc667f6e
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-09-24 19:18:07 -0300

    Showing the top 20 most popular movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,83 @@
-import React, { useEffect ,useState } from 'react'
-import Buscar from './componentes/Buscar'
+import React, { useEffect, useState } from "react";
+import Buscar from "./componentes/Buscar";
 
-const API_URL = 'https://api.themoviedb.org/3'
+const API_URL = "https://api.themoviedb.org/3";
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 const App = () => {
+  const [termoBusca, setTermoBusca] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [filmesLista, setFilmesLista] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [termoBusca,setTermoBusca] = useState("")
-
-  const [errMsg,setErrMsg] = useState("")
   const fetch_movies = async () => {
+    setIsLoading(true);
+    setErrMsg("");
     try {
-        const endpoint = `${API_URL}/discover/movie?sort_by=popularity.desc`
-        
-        const resp = await fetch(endpoint,API_OPTS)
+      const endpoint = `${API_URL}/discover/movie?sort_by=popularity.desc`;
 
-        alert(resp)
+      const resp = await fetch(endpoint, API_OPTS);
+
+      if (!resp.ok) {
+        throw new Error("Erro ao dar fetch em filmes");
+      }
+
+      const data = await resp.json();
+      if (data.Response === "false") {
+        setErrMsg(data.Error || "Erro ao dar fetch em filmes");
+        setFilmesLista([]);
+        return;
+      }
+      setFilmesLista(data.results || []);
     } catch (error) {
-      console.error(`Erro ao dar fetch nos filmes: ${error}`)
-      setErrMsg("Erro ao dar fetch. Por favor volte mais tarde.")
+      console.error(`Erro ao dar fetch nos filmes: ${error}`);
+      setErrMsg("Erro ao dar fetch. Por favor volte mais tarde.");
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-      fetch_movies()
-  },)
-
+    fetch_movies();
+  }, []);
 
   return (
     <main>
-      <div className='pattern'>
-          <div className="wrapper">
-            <header>
-              <img src="./hero.png" alt="" />
-              <h1>Encontre <span className="text-gradient">Filmes </span>que você gosta sem se aborecer</h1>
-                  <Buscar termoBusca={termoBusca} setTermoBusca={setTermoBusca}/>
-            </header>
-            <section className="all-movies">
-              <h2>Todos os filmes</h2>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="" />
+          <h1>
+            Encontre <span className="text-gradient">Filmes </span>que você
+            gosta sem se aborecer
+          </h1>
+          <Buscar termoBusca={termoBusca} setTermoBusca={setTermoBusca} />
+        </header>
+        <section className="all-movies">
+          <h2>Todos os filmes</h2>
 
-              {errMsg && <p className='text-red-700'>{errMsg}</p>}
-            </section>
-
-
-
-            <h1 className="text-white">{termoBusca}</h1>
-          </div>
-      
+          {isLoading ? (
+            <p className="text-white">Carregando...</p>
+          ) : errMsg ? (
+            <p className="text-red-600">{errMsg}</p>
+          ) : (
+            <ul>
+              {filmesLista.map((filme) => (
+                <p className="text-white">{filme.title}</p>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
