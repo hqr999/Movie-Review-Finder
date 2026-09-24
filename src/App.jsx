bf42830e3585222e24fbbb50893fc768906fbd87
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-09-24 16:20:06 -0300

    Established the basic tailwindcss on our react app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,9 @@
-import { useEffect, useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
-
-
-
-const Card = ({title}) => {
-
-  const [hasLiked, setHasLiked] = useState(false);
-  const [count, setcount] = useState(0)
-  useEffect(() => {
-      console.log(`${title} has been liked: ${hasLiked}`);
-  },[hasLiked]);
-  
-
-  useEffect(() => {
-    console.log('Card Rendered')
-  },[])
-
-  return(
-    <div className='card' onClick={() => setcount(count + 1)}>
-      <h2>{title} <br /> {count ? count : null}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ?  '❤️' : '💙'} 
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-    
-
-
-  return(
-    <div className='card-container'>
-      <Card title="Star Wars" rating={5} />
-      <Card title="Avatar" rating={4}/>
-      <Card title="Lion King" rating={3} />
-    </div>
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
   )
-
 }
-
 
 export default App
